134710685ab0c8ee3810935b1c4a1c3f95336af8
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-28 00:44:21 -0300

    Resolvi ejercicio 4

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,3 +1,21 @@
 /*4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado 
 una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
 */
+const numero = parseInt(prompt("Ingrese un número entero"));
+
+const paroImpar = (numero) => {
+  if (!isNaN(numero)) {
+    if (numero % 2 == 0) {
+      const respuesta = `El número ${numero} es un número par`;
+      document.writeln(respuesta);
+    } else {
+      const respuesta = `El número ${numero} es un número impar`;
+      document.writeln(respuesta);
+    }
+  }else {
+    document.writeln("El valor ingresado no es válido");
+  }
+  return respuesta;
+};
+
+paroImpar(numero)
