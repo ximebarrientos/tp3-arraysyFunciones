eef91a2f9d5482ddb204f3a8587a7726dfd6cc96
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-28 01:40:48 -0300

    resolvi ejercicio 6

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -2,3 +2,21 @@
 la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número 
 elegido por el usuario.
  */
+
+//pedir al usuario un numero
+//repetir el paso de escribir la tabla
+
+const numero=Number(prompt("Ingrese un número"))
+
+const tabla=(numero)=>{
+    if(!isNaN(numero)){
+        for(let i=1; i<11; i++){
+            const resultado=i*numero
+            document.writeln(`<br>${i} x ${numero}= ${i*numero}`)
+        }
+    }else{
+        document.writeln("No ingreso un valor válido")
+    }
+}
+
+tabla(numero)
