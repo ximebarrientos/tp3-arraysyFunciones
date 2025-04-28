b12bd90ced632f8f27c91ad7fc9807108a28b46c
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-28 01:42:14 -0300

    resolvi problema ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -4,13 +4,15 @@
 La fórmula del perímetro  es p = 2*(a +b)
  */
 
-const largo=Number(prompt("Ingrese el largo de un rectangulo"))
-const ancho=Number(prompt("Ingrese el ancho de un rectangulo"))
+const largo = Number(prompt("Ingrese el largo de un rectangulo"));
+const ancho = Number(prompt("Ingrese el ancho de un rectangulo"));
 
-const perimetro=(largo,ancho)=>{
-    if(!isNaN(largo) && !isNaN(ancho)){
-        return `El perimetro del rectangulo es: ${2*(largo+ancho)}`
-    }else{return "No ingresó un número válido"}
-    }
-const resultado=perimetro(largo,ancho)
-document.writeln(resultado)
+const perimetro = (largo, ancho) => {
+  if (!isNaN(largo) && !isNaN(ancho)) {
+    return `El perimetro del rectangulo es: ${2 * (largo + ancho)}`;
+  } else {
+    return "No ingresó un número válido";
+  }
+};
+const resultado = perimetro(largo, ancho);
+document.writeln(resultado);
